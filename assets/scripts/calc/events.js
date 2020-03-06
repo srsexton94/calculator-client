beb40a339d5ba064cb2b90dd9717dcc6799dd717
srsexton94 <srsexton94@gmail.com>
2020-03-06 18:17:09 -0500

    Adds initial button function (for numbers)
    Adds initial function to edit display appropriately upon a number
    selection
    Edits html ids/classes for selection needs
    Adds forgotten red color to C/AC buttons

--- assets/scripts/calc/events.js
+++ assets/scripts/calc/events.js
@@ -1,7 +1,20 @@
 'use strict'
+let equation = []
+
+const onNumber = event => {
+  event.preventDefault() // prevents page refresh
+  const numStr = event.target.id // sets a var with the number selected
+  const displayStr = $('#display').text() // sets a var with current display text
+
+  if (displayStr !== '0') { // if the display has anything other than a single 0
+    $('#display').text(displayStr + numStr) // ...add another digit
+  } else { // if the display is a single zero
+    $('#display').text(numStr) // ...replace the zero
+  }
+}
 
 const addHandlers = () => {
-  //
+  $('.number').on('click', onNumber)
 }
 
 module.exports = {
